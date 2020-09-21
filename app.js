85325a453e1fa521f02705b81f9c370a1861f39e
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-09-21 14:54:16 -0400

    Added check function to check total value of cards

--- app.js
+++ app.js
@@ -11,19 +11,25 @@ startBtn = document.querySelector('#start-btn')
 // Add event listener to button to start running the program
 startBtn.addEventListener('click', main)
 
+
 // Write a main function that runs the program.
 async function main() {
   let player = 0
   const deckId = await getId()
-  let userTotal = []
-
-  let card1 = drawCard(deckId, player)
-  let card2 = drawCard(deckId, player)
-  check()
+  let userHand = [
+    await drawCard(deckId, player),
+    await drawCard(deckId, player)
+  ]
+  let userTotal = await checkTotal(userHand, player)
+  console.log(userTotal)
 
   player = 1
-  drawCard(deckId, player)
-  drawCard(deckId, player)
+  let dealerHand = [
+    await drawCard(deckId, player),
+    await drawCard(deckId, player)
+  ]
+  let dealerTotal = await checkTotal(dealerHand, player)
+  console.log(dealerTotal)
 }
 
 // Write a function to capture the ID of a shuffled deck,
@@ -33,7 +39,7 @@ async function getId() {
     const response = await axios.get(url)
     //capture and return the deck id
     const deckId = response.data.deck_id
-    console.log(deckId)
+    // console.log(deckId)
     return deckId
   } catch (error) {
     console.log(`Error: ${error}`)
@@ -43,25 +49,37 @@ async function getId() {
 // Write a function that deals the cards
 
 
-// Write a function to draw 1 card
-async function drawCard(para1, para2) {
+// Write a function to draw 1 card, capturing and appending the img, resetting and returning the value.
+async function drawCard(para1, para2, para3) {
   let url = `https://deckofcardsapi.com/api/deck/${para1}/draw/?count=1`
   try {
     const response = await axios.get(url)
     let card = response.data.cards
-    console.log(card)
+    // console.log(card)
 
+    // grab containers for user container and dealer container from html
     userContainer = document.querySelector('#user-card-container')
     dealerContainer = document.querySelector('#dealer-card-container')
 
-    let value = card[0].value
+    // create img element to put the card img in
     const cardImg = document.createElement('img')
     cardImg.src = `${card[0].image}`
 
+    // append to container depending on which player is playing
     if (para2 === 0) {
       userContainer.append(cardImg)
     } else {
       dealerContainer.append(cardImg)
+    }
+    // get the value of the card
+    let value = card[0].value
+    // if the value is king queen or jack reset to 10
+    if (value === 'JACK' || value === 'QUEEN' || value === 'KING') {
+      value = "10"
+    } else if (para3 < 21 && value === 'ACE') {
+      value = "11"
+    } else if (para3 > 21 && value === 'ACE') {
+      value = "1"
     }
     return value
   } catch (error) {
@@ -72,5 +90,25 @@ async function drawCard(para1, para2) {
 // Write a function that checks the value of each players cards
 async function checkTotal(para1, para2) {
 
-}
+  let reactBox = document.querySelector('#reaction-container')
+  let userTotalContainer = document.querySelector('#user-total-container')
+  let dealerTotalContainer = document.querySelector('#dealer-total-container')
 
+  let nums = para1.map(Number)
+  let total = 0
+  nums.forEach((num) => {
+    total += num
+  })
+
+  if (para2 === 0) {
+    userTotalContainer.innerHTML = `<p>${total}</p>`
+  } else {
+    dealerTotalContainer.innerHTML = `<p>${total}</p>`
+  }
+  if (total === 21) {
+    reactBox.innerText = `<h1>Natural</h1>`
+  } else if (total > 21) {
+    reactBox.innerText = `<h1>Bust</h1>`
+  }
+  return total
+}
