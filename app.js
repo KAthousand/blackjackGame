98a0700b08c338bedc10ad642513b2f68b60ff48
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-09-21 16:28:58 -0400

    Hit Me button works

--- app.js
+++ app.js
@@ -7,29 +7,40 @@
 
 
 // Capture the start button.
-startBtn = document.querySelector('#start-btn')
+const startBtn = document.querySelector('#start-btn')
 // Add event listener to button to start running the program
 startBtn.addEventListener('click', main)
 
 
 // Write a main function that runs the program.
 async function main() {
-  let player = 0
+
   const deckId = await getId()
+
+  let player = 0
+  let userTotal = 0
   let userHand = [
-    await drawCard(deckId, player),
-    await drawCard(deckId, player)
+    await drawCard(deckId, player, userTotal),
+    await drawCard(deckId, player, userTotal)
   ]
-  let userTotal = await checkTotal(userHand, player)
+  userTotal += await checkTotal(userHand, player, userTotal)
   console.log(userTotal)
 
   player = 1
+  let dealerTotal = 0
   let dealerHand = [
-    await drawCard(deckId, player),
-    await drawCard(deckId, player)
+    await drawCard(deckId, player, dealerTotal),
+    await drawCard(deckId, player, dealerTotal)
   ]
-  let dealerTotal = await checkTotal(dealerHand, player)
+  dealerTotal += await checkTotal(dealerHand, player, dealerTotal)
   console.log(dealerTotal)
+
+  player = 0
+  const hitBtn = document.querySelector('#hit')
+  hitBtn.addEventListener('click', (async () => {
+    userHand.push(await drawCard(deckId, player, userTotal))
+    userTotal += await checkTotal(userHand, player, userTotal)
+  }))
 }
 
 // Write a function to capture the ID of a shuffled deck,
@@ -76,39 +87,54 @@ async function drawCard(para1, para2, para3) {
     // if the value is king queen or jack reset to 10
     if (value === 'JACK' || value === 'QUEEN' || value === 'KING') {
       value = "10"
+      // if the total (para3) is less than 21, ace === 11
     } else if (para3 < 21 && value === 'ACE') {
       value = "11"
+      // if the total (para3) is greater than 21, ace === 11
     } else if (para3 > 21 && value === 'ACE') {
       value = "1"
     }
+    console.log(`Value of userTotal : ${para3}`)
+    console.log(`Value of Card : ${value}`)
     return value
   } catch (error) {
     console.log(`Error: ${error}`)
   }
 }
 
-// Write a function that checks the value of each players cards
-async function checkTotal(para1, para2) {
+// Write a function that checks the value of each players cards-----------------------------
+async function checkTotal(playerHand, playerNum, playerTotal) {
 
   let reactBox = document.querySelector('#reaction-container')
   let userTotalContainer = document.querySelector('#user-total-container')
   let dealerTotalContainer = document.querySelector('#dealer-total-container')
 
-  let nums = para1.map(Number)
+  console.log(`Player Hand: ${playerHand}`)
+  let nums = playerHand.map(Number)
   let total = 0
   nums.forEach((num) => {
     total += num
+    console.log(`line 177 ${total} `)
   })
+  // total += playerTotal
+  console.log(`PlayerTotal : ${total}`)
 
-  if (para2 === 0) {
-    userTotalContainer.innerHTML = `<p>${total}</p>`
+
+  if (playerNum === 0) {
+    userTotalContainer.innerHTML = `<p>User Total: ${total}</p>`
   } else {
-    dealerTotalContainer.innerHTML = `<p>${total}</p>`
+    dealerTotalContainer.innerHTML = `<p>Dealer Total: ${total}</p>`
   }
   if (total === 21) {
-    reactBox.innerText = `<h1>Natural</h1>`
+    reactBox.innerHTML = `<h1>Natural</h1>`
   } else if (total > 21) {
-    reactBox.innerText = `<h1>Bust</h1>`
+    reactBox.innerHTML = `<h1>Bust</h1>`
   }
   return total
+}
+
+// Write a function the hit me button!
+async function hitMe() {
+  await drawCard()
+  await checkTotal()
 }
